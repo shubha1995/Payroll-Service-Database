4e15a8a360bf61a5cc09f9ca7216b01349c7a89f
shubhasankar <ssbhaumikdmr@gmail.com>
2021-07-29 19:47:28 +0530

    Added: Ability to create a payroll service database

--- src/main/java/com/payroll/Emp_payRoll.java
+++ src/main/java/com/payroll/Emp_payRoll.java
@@ -1,9 +1,34 @@
 package com.payroll;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class Emp_payRoll {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Pay Roll Program");
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_services";
+		String userName = "root";
+		String password = "shubha@1995.";
+		Connection con;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded!");
+
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+
+		}
+
+		try {
+			System.out.println("Connecting to database:" + jdbcURL);
+			con = (Connection) DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection is Successful!!!!" + con);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
+
 }
